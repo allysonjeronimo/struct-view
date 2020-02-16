b1138f7a335f2963a440ffcc7b4577b84b007bd1
Allyson Jeronimo <allysonjeronimo@gmail.com>
2020-02-16 01:01:23 -0300

    Implemented current cursor node

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 state = {
     array: [],
     size: 10,
+    delay: 50,
     dom: {
         content: document.getElementById('content')
     },
-    colors:[
+    colors: [
         '#103215',
-        '#15421B', 
+        '#15421B',
         '#1A5322',
         '#206329',
         '#257430',
@@ -32,24 +33,24 @@ function insert(current) {
     if (current < state.array.length) {
         state.array[current] = random()
         render(current, true)
-        setTimeout(() => insert(current + 1), 50)
+        setTimeout(() => insert(current + 1), state.delay)
     }
     return
 }
 
 function remove(current, deleteNode) {
     if (current >= 0) {
-        
-        if(deleteNode){
+
+        if (deleteNode) {
             delete state.array[current]
             renderRemove(current)
         }
-        else{
+        else {
             state.array[current] = undefined
             render(current, true)
         }
-            
-        setTimeout(() => remove(current - 1, deleteNode), 50)
+
+        setTimeout(() => remove(current - 1, deleteNode), state.delay)
     }
     return
 }
@@ -63,7 +64,7 @@ function renderAll() {
 function renderRecursive(current) {
     if (current < state.array.length) {
         render(current)
-        setTimeout(() => renderRecursive(current + 1), 50)
+        setTimeout(() => renderRecursive(current + 1), state.delay)
     }
     return
 }
@@ -72,30 +73,37 @@ function renderRemove(i) {
     document.getElementById(`node-${i}`).remove()
 }
 
+function setCursor(i) {
+    if (i >= 0 && i < state.array.length) {
+        document.getElementById(`content-node-${i}`).classList.add('current-node')
+    }
+}
+
 function render(i, replace = false) {
 
     let filled = state.array[i] !== undefined
 
     let background = filled ? `style='background: ${state.colors[state.array[i]]}'` : ''
 
-    let classes = filled ? 'filled-node current-node' : 'empty-node current-node'
+    let classes = filled ? 'filled-node' : 'empty-node'
 
     if (replace) {
-        // filling in array
+        // filling or emptying array
         document.getElementById(`node-${i}`).innerHTML = `
-        <div class="${classes}" ${background}>
+        <div id="content-node-${i}" class="${classes}" ${background}>
             ${filled ? state.array[i] : '-'}
-            </div>
+        </div>
         <div class='node-index'>
             ${i}
         </div>
         `
+        setCursor(filled ? i + 1: i - 1)
     }
     else {
         // creating a new array
         state.dom.content.innerHTML += `
-    <div class='node' id='node-${i}'>
-                <div class=${classes} ${background}>
+        <div class='node' id='node-${i}'>
+                <div id='content-node-${i}' class=${classes} ${background}>
                     ${filled ? state.array[i] : '-'}
                 </div>
                 <div class='node-index'>
@@ -105,17 +113,21 @@ function render(i, replace = false) {
     `
     }
 
+
+
 }
 
 function empty() {
-    remove(state.array.length-1)
+    remove(state.array.length - 1)
 }
 
 function handleEmpty() {
+    setCursor(state.array.length-1)
     empty()
 }
 
 function handleFill() {
+    setCursor(0)
     insert(0)
 }
 
@@ -125,8 +137,8 @@ function handleCreate() {
     renderRecursive(0)
 }
 
-function handleDestroy(){
-    remove(state.array.length-1, true)
+function handleDestroy() {
+    remove(state.array.length - 1, true)
 }
 
 
